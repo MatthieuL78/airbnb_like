e7693e930b53a99cdd745cf081834ec6063f9cb4
geraldy1st <geraldy.leondas@gmail.com>
2018-05-28 18:49:44 +0200

    css fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  root 'home#index'
-=======
+
 
   root 'home#index'
 
   resources :items
 
->>>>>>> master
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 0) do
-=======
 ActiveRecord::Schema.define(version: 2018_05_28_155528) do
 
   create_table "items", force: :cascade do |t|
@@ -23,6 +20,5 @@ ActiveRecord::Schema.define(version: 2018_05_28_155528) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
->>>>>>> master
 
 end
