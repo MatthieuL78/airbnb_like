2cd69ad9b454facbee7df938392329a25a6eaa6b
geraldy1st <geraldy.leondas@gmail.com>
2018-05-29 17:48:18 +0200

    footer ok

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'home#index'
-
-  get 'home/show', to: 'home#show'
+  
   resources :items
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
