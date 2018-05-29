688cde469fffc0fb0bf56fe71742eb34061873e6
geraldy1st <geraldy.leondas@gmail.com>
2018-05-29 22:59:00 +0200

    22h58

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  root 'home#index'
 
   devise_for :users
-  root 'home#index'
-  
+  get "users/sign_up", to: "users#new", as: "sign_up"
+  get "users/sign_in", to: 'sessions#new', as: "sign_in"
+
   resources :items
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
