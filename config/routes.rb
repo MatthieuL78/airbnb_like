7171ad7e6c51427bbe8ddf7bab1def1721b90b90
geraldy1st <geraldy.leondas@gmail.com>
2018-05-31 01:52:33 +0200

    fixing bugs

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,11 @@ Rails.application.routes.draw do
 
   get "users/sign_up", to: "users#new", as: "sign_up"
   get "users/sign_in", to: 'sessions#new', as: "sign_in"
+  get "users/edit", to: 'sessions#edit', as: "edit"
 
   post '/', to: 'carts#destroy_item'
 
   resources :items, :carts
+  get "carts/:id", to: "carts#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
