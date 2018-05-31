89267d8e595cc6459028c8228d3d88f41b0f7386
MatthieuL78 <matthieu.lu@outlook.fr>
2018-05-31 10:23:08 +0200

    some opti

---
+++ app/services/cart_user.rb
@@ -0,0 +1,4 @@
+def user_cart
+  # @cart = Cart.user.find(current_user.id)
+  p 'PROUT AND CACA'
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
+  
   get "home/:id", to: "home#show", as: "show"
 
   devise_for :users
-
-  get "users/sign_up", to: "users#new", as: "sign_up"
-  get "users/sign_in", to: 'sessions#new', as: "sign_in"
 
   post '/', to: 'carts#destroy_item'
 
