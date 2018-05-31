ad1483977e7defa80c0954f04ba6051fea1e272a
MatthieuL78 <matthieu.lu@outlook.fr>
2018-05-31 17:34:19 +0200

    stripe un peu

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   get "home/:id", to: "home#show", as: "show"
 
   devise_for :users
+  resources :items, :carts, :charges
 
   post '/:id/item/:item', to: 'carts#add_item', as: 'add_item'
   post '/:id', to: 'carts#destroy_item', as: 'destroy_item'
 
-  resources :items, :carts, :charges
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
