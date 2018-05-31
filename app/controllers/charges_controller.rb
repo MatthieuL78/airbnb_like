3fe016665036ce9b2f8667869a0d5ad4b9b8b309
MatthieuL78 <matthieu.lu@outlook.fr>
2018-05-31 18:05:01 +0200

    empty cart

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,5 +1,12 @@
 class ChargesController < ApplicationController
 	def new
-		p @cart
 	end
+
+	def create
+	  cart = Cart.find_by(user_id: current_user.id)
+	  Item.all.each do |item|
+        item.carts.delete(cart)
+      end
+	end
+
 end
